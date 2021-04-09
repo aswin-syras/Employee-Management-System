b538deccb16fb0e6230c33c9e424ea436cb82b40
vaishaksu <vaishakashik@gmail.com>
2021-04-08 22:59:19 -0400

    [Commiting Login and admin page header modification]

--- login.py
+++ login.py
@@ -10,7 +10,6 @@ from wtforms.fields.html5 import DateTimeLocalField, EmailField
 import logging
 from random import randrange
 
-
 login = Blueprint("login", __name__, static_folder="static", template_folder="templates")
 
 # Login table name
@@ -30,27 +29,13 @@ class LoginForm(FlaskForm):
     accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
 
 
-def generating_random_id_login():
-    # Generating a new random id for employees
-    i = 1
-    all_login_info = database_connection.login_table(login_table_name)
-    while (i > 0):
-        random_id_generator = randrange(1, 1000000)
-
-        # Fetch only the Id only for comparing
-        result = map(lambda x: x["_id"], all_login_info)
-        not_in_list = random_id_generator not in list(result)
-        if (not_in_list):
-            i = 0
-            return random_id_generator
-        else:
-            i = 1
-
 @login.route("/")
 def login_info():
+    if 'username' in session:
+        session.clear() # Clea the session
     form = LoginForm()
     return render_template("shared-component/login.html",
-                           form1=form)
+                           form=form)
 
 
 @login.route("/register")
@@ -59,7 +44,7 @@ def register():
     return render_template("shared-component/RegistrationForm.html",
                            form1=form)
 
-@login.route("/signupPOST", methods=["GET","POST"])
+@login.route("/signupPOST", methods=["GET", "POST"])
 def signupPostData():
     error = []
     if request.form.get('password') != request.form.get('confirm'):
@@ -85,25 +70,43 @@ def signupPostData():
         print(request.form)
         return redirect(url_for('login.login_info'))
 
-def loginComparison ( all_login_info ):
+def loginComparison(all_login_info):
     count = 1
     index = 0
     boolValue = False
     print(request.form)
     while count > 0:
         if index < len(all_login_info):
-            if all_login_info[index]["username"] != request.form.get('username') and all_login_info[index]["password"] != request.form.get('password'):
-                count += 1
-                boolValue = False
-            else:   # SUCCESS
+            if all_login_info[index]["username"] == request.form.get('username') and all_login_info[index]["password"] == request.form.get('password'):
                 count = 0
                 boolValue = True
-                return boolValue
+                data = {
+                    'booleanValue': boolValue,
+                    'one_data': all_login_info[index]
+                }
+                return data
+            else:  # SUCCESS
+                count += 1
+                boolValue = False
             index += 1
-
         else:
             count = 0
 
+def generating_random_id_login():
+    # Generating a new random id for employees
+    i = 1
+    all_login_info = database_connection.login_table(login_table_name)
+    while i > 0:
+        random_id_generator = randrange(1, 1000000)
+
+        # Fetch only the Id only for comparing
+        result = map(lambda x: x["_id"], all_login_info)
+        not_in_list = random_id_generator not in list(result)
+        if not_in_list:
+            i = 0
+            return random_id_generator
+        else:
+            i = 1
 
 
 @login.route("/login_validation", methods=["POST"])
@@ -113,17 +116,26 @@ def login_validationPost():
     login_check = loginComparison(all_login_info)
 
     error = []
+    print("user12: ", login_check)
     if login_check:
+        session['username'] = login_check['one_data']['username']
+        session['first_name'] = login_check['one_data']['first_name']
+        session['last_name'] = login_check['one_data']['last_name']
+        session['is_admin'] = login_check['one_data']['is_admin']
+        session['_id'] = login_check['one_data']['_id']
+        session['email_address'] = login_check['one_data']['email_address']
+
+        if login_check['one_data']['is_admin']:
+            print("Reditct him to Sdmin oage: ", session)
+
+            return redirect(url_for("admin.adminHome"))
+        else:
+            print("to notmal emp page")
+            # TODO: Redirect to Employee page
         return "Success"
     else:
         error.append("Username and password doesn't match")
-        form=LoginForm()
+        form = LoginForm()
         return render_template("shared-component/login.html",
-                               form1=form,
+                               form=form,
                                error=error)
-
-
-
-
-
-
